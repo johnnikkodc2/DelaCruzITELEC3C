bcc0c3c6d835086bb986aa188391f8d56c8efa04
johnnikkodc2 <johnnikko.delacruz.cics@ust.edu.ph>
2023-11-29 17:11:34 +0800

    done

--- resources/views/admin/category/category.blade.php
+++ resources/views/admin/category/category.blade.php
@@ -12,12 +12,33 @@
     
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-6">
                 <div class="container">
                     <div class="row">
-                        <div class="col-md-8">
-                            <div class="card">
+                        <div class="col-md-12 mb-4">
+                            <div class="card p-4">
+                                <h2 class="mb-4">Add Category</h2>
+                                <form method="POST" action="{{ route('categories.store') }}" enctype="multipart/form-data">
+                                    @csrf
+                                    <div class="mb-3">
+                                        <label for="category_name" class="form-label">Category Name</label>
+                                        <input type="text" class="form-control" name="category_name" id="category_name">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="category_image" class="form-label">Category Image</label>
+                                        <input type="file" class="form-control" name="category_image" id="category_image" accept="image/*">
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </form>
+                            </div>
+                        </div>
+                        
+                        <div class="col-md-12 mb-4">
+                            <div class="card p-4">
+                                <h2 class="mb-4">Categories</h2>
+                         
                                 <table class="table table-striped">
+                                   
                                     <thead>
                                         <tr>
                                             <th>ID</th>
@@ -28,8 +49,9 @@
                                             <th>Photo</th>
                                             <th>Edit</th>
                                             <th>Delete</th>
-                                        </tr>   
+                                        </tr>
                                     </thead>
+                                   
                                     <tbody>
                                         @foreach ($categories as $category)
                                         <tr>
@@ -38,49 +60,30 @@
                                             <td>{{ $category->user_id }}</td>
                                             <td>{{ $category->created_at }}</td>
                                             <td>{{ $category->updated_at }}</td>
-                                            <td> <img src="{{ asset('img/' . $category->category_image) }}" alt=""></td>
+                                            <td><img src="{{ asset('img/' . $category->category_image) }}" style="max-width: 200px; max-height: 200px;" alt=""></td>
                                             <td>
                                                 <a href="{{url('/all/editCategory/edit/'.$category->id)}}">
                                                     <i class="fas fa-edit"></i>
                                                 </a>
-
-                                               
                                             </td>
-                                            <td> <a href="{{url('/all/category/delete/'.$category->id)}}">
-                                               
-                                                 <i class="fas fa-trash-alt"></i>
-                                             </a></td>
+                                            <td>
+                                                <a href="{{url('/all/category/delete/'.$category->id)}}">
+                                                    <i class="fas fa-trash-alt"></i>
+                                                </a>
+                                            </td>
                                         </tr>
                                         @endforeach
                                     </tbody>
                                 </table>
                             </div>
                         </div>
-                        <div class="col-md-4">
-                            <div class="card">
-                                <h1>Add Category</h1>
-                                <form method="POST"   action="{{ route('categories.store') }}"  enctype="multipart/form-data">
-                                    @csrf
-                                    <div class="mb-3">
-                                        <label for="category_name" class="form-label">Category Name</label>
-                                        <input type="text" class="form-control" name="category_name" id="category_name">
-                                    </div>
-                                    <div class="mb-3">
-                                        <label for="category_image" class="form-label">Category Image</label>
-                                        <input type="file" class="form-control" name="category_image" id="category_image" accept="image/*">
-                                    </div>
-                                    
-                               
-                                   
-                                    <button type="submit" class="btn btn-primary">Submit</button>
-                                
-                                </form>
-                                <br>
-                            </div>
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card">
+                        
+                        <div class="col-md-12">
+                            <div class="card p-4">
+                                <h2 class="mb-4">Deleted Categories</h2>
+                             
                                 <table class="table table-striped">
+                                  
                                     <thead>
                                         <tr>
                                             <th>ID</th>
@@ -90,8 +93,9 @@
                                             <th>Photo</th>
                                             <th>Restore</th>
                                             <th>Delete</th>
-                                        </tr>   
+                                        </tr>
                                     </thead>
+                             
                                     <tbody>
                                         @foreach ($deletedCategories as $deletedCategory)
                                         <tr>
@@ -99,17 +103,17 @@
                                             <td>{{ $deletedCategory->category_name }}</td>
                                             <td>{{ $deletedCategory->user_id }}</td>
                                             <td>{{ $deletedCategory->created_at }}</td>
-                                            <td> <img src="{{ asset('img/' . $deletedCategory->category_image) }}" alt=""></td>
+                                            <td><img src="{{ asset('img/' . $deletedCategory->category_image) }}" style="max-width: 200px; max-height: 200px;" alt=""></td>
                                             <td>
                                                 <a href="{{ url('/all/category/restore/'.$deletedCategory->id) }}">
                                                     <i class="fas fa-trash-restore"></i>
                                                 </a>
                                             </td>
-                                            
-                                            <td> <a href="{{url('/all/category/forceDelete/'.$deletedCategory->id)}}">
-                                               
-                                                 <i class="fas fa-trash-alt"></i>
-                                             </a></td>
+                                            <td>
+                                                <a href="{{url('/all/category/forceDelete/'.$deletedCategory->id)}}">
+                                                    <i class="fas fa-trash-alt"></i>
+                                                </a>
+                                            </td>
                                         </tr>
                                         @endforeach
                                     </tbody>
@@ -120,7 +124,5 @@
                 </div>
             </div>
         </div>
-        
     </div>
-   
 </x-app-layout>
--- resources/views/admin/category/editCategory.blade.php
+++ resources/views/admin/category/editCategory.blade.php
@@ -11,22 +11,15 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
-
-    <style>
-        body {
-            padding-top: 70px; 
-        }
-    </style>
 </head>
 
-<body>
+<body style="background-color: white;">
     <div class="container mt-5">
         <div class="row">
-            <div class="col-md-8 mx-auto"> 
-             
+            <div class="col-md-8 mx-auto">
                 <h1 class="font-semibold text-xl text-gray-800 leading-tight mb-4">Edit Category</h2>
 
-                    <form method="POST" action="{{url('all/editCategory/update/'.$categories->id)}}"  enctype="multipart/form-data">
+                <form method="POST" action="{{ url('all/editCategory/update/'.$categories->id) }}" enctype="multipart/form-data">
                     @csrf
                     <div class="mb-3">
                         <label for="category_name" class="form-label">Category Name</label>
@@ -44,8 +37,7 @@
                             onchange="updateImagePreview(event)">
                     </div>
                     <button type="submit" class="btn btn-primary">Update Category</button>
-                    <button  class="btn btn-primary" onclick="{{ url('/all/category') }}" >Go Back</button>
-                    
+                    <a href="{{ url('/all/category') }}" class="btn btn-primary">Go Back</a>
                 </form>
             </div>
         </div>
